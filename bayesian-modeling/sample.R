399dcb2fe855f778638d65d9e19005a78ca12094
Sreejith Sreekumar <sreejith2904@gmail.com>
2022-09-16 12:40:40 -0700

    bayesian experiments

---
+++ bayesian-modeling/sample.R
@@ -0,0 +1,15 @@
+
+
+mu = 0.1
+sd = 1
+
+# n pseudo random samples from  a normal distribution
+rnorm(10, mu, sd)
+
+# evaluate pdf at mean mu and standard deviation sigma sd
+x = 0.4
+dnorm(x, mu, sd)
+x = 0.8
+dnorm(x, mu, sd)
+  x = 4
+dnorm(x, mu, sd)
